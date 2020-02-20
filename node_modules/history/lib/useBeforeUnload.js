7eeb2d6f5f069605afdaf48b21bb10464afbad66
Dev <clarkpeace.adams@gmail.com>
2020-02-20 06:15:54 +0000

    added dependencies to package.json

--- node_modules/history/lib/useBeforeUnload.js
+++ node_modules/history/lib/useBeforeUnload.js
@@ -1,6 +1,8 @@
 'use strict';
 
-exports.__esModule = true;
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
 
 var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
 
